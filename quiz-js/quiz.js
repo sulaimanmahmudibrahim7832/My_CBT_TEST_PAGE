552a5b1d11c6e377a2c513b794b5426abdb279f8
Sulaiman Mahmud Ibrahim <sulaimanmahmudibrahim7832@gmail.com>
2026-05-08 18:58:28 +0100

    Refactor quiz.js to improve structure and readability

--- quiz-js/quiz.js
+++ quiz-js/quiz.js
@@ -12,15 +12,6 @@ document.querySelector(".goto-container").style.display = "none";
 
 
 let GotoButtonsHtml = '';
-
-
-})
-  
-};
-
-
-
-
  const QoptionA = document.querySelector(".optionA");
  const QoptionB = document.querySelector(".optionB");
  const QoptionC = document.querySelector(".optionC");
